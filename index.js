4680ce3703d4e6c1ecc6673325aaba701a0e6622
Juliza <zepeda.juliza@gmail.com>
2023-05-29 16:27:42 -0300

    creates tasks vairables

---
+++ index.js
@@ -0,0 +1,14 @@
+const newTaskNameInput = document.getElementById("task-name");
+const name = newTaskNameInput.value;
+
+
+const newTaskDescription = document.getElementById('task-description');
+const description = newTaskDescription.value;
+
+
+const newAssignedTo = document.getElementById('assigned-to');
+const assigned = newAssignedto.value;
+
+
+const newDueDate = document.getElementById('due-date');
+const dueDate = newDueDate.value;
