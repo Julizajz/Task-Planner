b9bcc857a6f2dcbebad1faec7aa80cd0ef1b19c7
Juliza <zepeda.juliza@gmail.com>
2023-05-31 19:07:57 -0300

    add js folder

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const validFormFieldInput = () => {
+
+}
+
+
+// const newTaskDescription = document.getElementById('taskDescription');
+// const description = newTaskDescription.value;
+
+
+// const newAssignedTo = document.getElementById('assignedTo');
+// const assigned = newAssignedto.value;
+
+
+// const newDueDate = document.getElementById('dueDate');
+// const dueDate = newDueDate.value;
