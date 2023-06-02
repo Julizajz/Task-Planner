a236faadc8e567112dc64cfaabb1f7ef1473fad2
Juliza <zepeda.juliza@gmail.com>
2023-06-02 00:02:10 -0300

    oragnized variables and input values

--- js/index.js
+++ js/index.js
@@ -1,15 +1,37 @@
-const validFormFieldInput = () => {
 
-}
-
-
-// const newTaskDescription = document.getElementById('taskDescription');
-// const description = newTaskDescription.value;
+// task form selector
+const newTaskForm = document.getElementById('newTaskForm')
 
 
-// const newAssignedTo = document.getElementById('assignedTo');
-// const assigned = newAssignedto.value;
+newTaskForm.addEventListener('submit', (event) => {
+
+    // prevent default action
+    event.preventDefault();
+
+    // element input selectors 
+    const newTaskNameInput = document.getElementById('taskName');
+    const newTaskDescriptionInput = document.getElementById('taskDescription');
+    const newAssignedToInput = document.getElementById('assignedTo');
+    const newDueDateInput = document.getElementById('dueDate');
+
+    // input values
+    const name = newTaskNameInput.value;
+    const description = newTaskDescriptionInput.value;
+    const assignedTo = newAssignedToInput.value;
+    const dueDate = newDueDateInput.value;
+})
 
 
-// const newDueDate = document.getElementById('dueDate');
-// const dueDate = newDueDate.value;
+
+
+
+
+
+
+// element input selectors 
+// const newTaskNameInput = document.getElementById('taskName');
+// const newTaskDescriptionInput = document.getElementById('taskDescription');
+// const newAssignedToInput = document.getElementById('assignedTo');
+// const newDueDateInput = document.getElementById('dueDate');
+
+
